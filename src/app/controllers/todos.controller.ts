b519d0673d5a0b2c12c5c8e0675ba6207bd4b565
Mehmet <mehmetext68@gmail.com>
2024-11-09 21:56:04 +0300

    feat: update TodosController structure and add wait function

--- src/app/controllers/todos.controller.ts
+++ src/app/controllers/todos.controller.ts
@@ -1,11 +1,23 @@
+import { Request, Response } from "express";
+import ApiResponse from "@/lib/core/api-response";
+
 import todos from "@/lib/static/todos";
+import wait from "@/lib/utils/wait.util";
 
 export default class TodosController {
-  static async getTodos() {
-    return todos;
+  static async getTodos(req: Request, res: Response) {
+    await wait(200);
+    return ApiResponse.success(res, todos);
   }
 
-  static async getTodoById(id: string) {
-    return todos.find((todo) => todo.id === id);
+  static getTodoById(req: Request<{ id: string }>, res: Response) {
+    const { id } = req.params;
+    const todo = todos.find((todo) => todo.id === id);
+
+    if (!todo) {
+      return ApiResponse.notFound(res, "Todo not found");
+    }
+
+    return ApiResponse.success(res, todo);
   }
 }
--- src/app/routes/todos.route.ts
+++ src/app/routes/todos.route.ts
@@ -1,23 +1,9 @@
 import { TodosController } from "@/controllers";
-import ApiResponse from "@/lib/core/api-response";
 import { Router, Request } from "express";
 
 const router: Router = Router();
 
-router.get("/", async (req, res) => {
-  const todos = await TodosController.getTodos();
-  return ApiResponse.success(res, todos);
-});
-
-router.get("/:id", async (req: Request<{ id: string }>, res) => {
-  const { id } = req.params;
-  const todo = await TodosController.getTodoById(id);
-
-  if (!todo) {
-    return ApiResponse.notFound(res, "Todo not found");
-  }
-
-  return ApiResponse.success(res, todo);
-});
+router.get("/", TodosController.getTodos);
+router.get("/:id", TodosController.getTodoById);
 
 export default router;
