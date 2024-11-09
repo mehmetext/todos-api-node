5847d178df67f63200daab147e68585788156c44
Mehmet <mehmetext68@gmail.com>
2024-11-09 17:52:38 +0300

    fix: add alias for js

---
+++ src/app/lib/static/todos.ts
@@ -0,0 +1,45 @@
+const todos: ITodo[] = [
+  {
+    id: 1,
+    title: "Buy milk",
+    content: "Buy milk from the store",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Buy bread",
+    content: "Buy bread from the store",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Call dentist",
+    content: "Make an appointment for dental check-up",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Complete project report",
+    content: "Finish quarterly project documentation",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "Exercise",
+    completed: false,
+  },
+  {
+    id: 6,
+    title: "Pay bills",
+    content: "Pay electricity and water bills",
+    completed: true,
+  },
+  {
+    id: 7,
+    title: "Clean house",
+    content: "Vacuum and dust the living room",
+    completed: false,
+  },
+];
+
+export default todos;
--- src/app/routes/todos.route.ts
+++ src/app/routes/todos.route.ts
@@ -1,11 +1,10 @@
+import todos from "@/lib/static/todos";
 import { Router } from "express";
 
 const router: Router = Router();
 
 router.get("/", (req, res) => {
-  res.json({
-    message: "Todos endpoint",
-  });
+  res.json(todos);
 });
 
 export default router;
