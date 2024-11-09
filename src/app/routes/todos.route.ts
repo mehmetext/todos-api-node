1125993216215484b29a64825d9e873cb1e7dea6
Mehmet <mehmetext68@gmail.com>
2024-11-09 23:01:02 +0300

    Update todos.route.ts

--- src/app/routes/todos.route.ts
+++ src/app/routes/todos.route.ts
@@ -1,11 +1,10 @@
 import { TodosController } from "@/controllers";
-import { authMiddleware } from "@/middlewares";
 import { Router } from "express";
 
 const router: Router = Router();
 
 router.get("/", TodosController.getTodos);
 router.get("/:id", TodosController.getTodoById);
-router.post("/", authMiddleware, TodosController.createTodo);
+router.post("/", /* authMiddleware, */ TodosController.createTodo);
 
 export default router;
