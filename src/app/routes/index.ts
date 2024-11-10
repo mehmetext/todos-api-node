d334f0f1da43de53a22eb4babc8f253307685dea
Mehmet <mehmetext68@gmail.com>
2024-11-10 15:09:12 +0300

    fix: update todos route constant

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,7 +5,7 @@ import todos from "./todos.route";
 
 const router: Router = Router();
 
-router.use(API.ROUTES.BASE, todos);
+router.use(API.ROUTES.TODOS, todos);
 router.use(API.ROUTES.AUTH, auth);
 
 export default router;
