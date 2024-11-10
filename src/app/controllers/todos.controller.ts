39eda3d666a0f747731bc2929d731fd7c4f6f5f7
Mehmet <mehmetext68@gmail.com>
2024-11-10 14:00:14 +0300

    feat: add todo service

--- src/app/controllers/todos.controller.ts
+++ src/app/controllers/todos.controller.ts
@@ -1,11 +1,10 @@
 import ApiResponse from "@/lib/core/api-response";
-import { DEFAULT_PAGE_SIZE } from "@/lib/core/options";
-import prisma from "@/lib/core/prisma";
 import {
   CreateTodoInput,
   GetTodosInput,
   UpdateTodoInput,
 } from "@/lib/validations";
+import { TodoService } from "@/services";
 import { Request, Response } from "express";
 
 export default class TodosController {
@@ -13,78 +12,14 @@ export default class TodosController {
     req: Request<unknown, unknown, unknown, GetTodosInput["query"]>,
     res: Response
   ) {
-    const { sort, q, page = 1 } = req.query;
-    const skip = (page - 1) * DEFAULT_PAGE_SIZE;
-
-    const [todos, total] = await Promise.all([
-      prisma.todo.findMany({
-        select: {
-          id: true,
-          title: true,
-          content: true,
-          completed: true,
-          createdAt: true,
-          updatedAt: true,
-        },
-        where: {
-          deletedAt: null,
-          userId: req.user!.id,
-          ...(q && {
-            OR: [
-              { title: { contains: q, mode: "insensitive" } },
-              { content: { contains: q, mode: "insensitive" } },
-            ],
-          }),
-        },
-        orderBy: {
-          ...(sort === "ascByCreatedAt" && { createdAt: "asc" }),
-          ...(sort === "descByCreatedAt" && { createdAt: "desc" }),
-          ...(sort === "ascByUpdatedAt" && { updatedAt: "asc" }),
-          ...(sort === "descByUpdatedAt" && { updatedAt: "desc" }),
-          ...(sort === "ascByCompleted" && { completed: "asc" }),
-          ...(sort === "descByCompleted" && { completed: "desc" }),
-          ...(sort === "ascByTitle" && { title: "asc" }),
-          ...(sort === "descByTitle" && { title: "desc" }),
-          ...(sort === "ascByContent" && { content: "asc" }),
-          ...(sort === "descByContent" && { content: "desc" }),
-        },
-        skip,
-        take: DEFAULT_PAGE_SIZE,
-      }),
-      prisma.todo.count({
-        where: {
-          deletedAt: null,
-          userId: req.user!.id,
-          ...(q && {
-            OR: [
-              { title: { contains: q, mode: "insensitive" } },
-              { content: { contains: q, mode: "insensitive" } },
-            ],
-          }),
-        },
-      }),
-    ]);
-
-    return ApiResponse.success(res, {
-      pagination: {
-        total,
-        totalPage: Math.ceil(total / DEFAULT_PAGE_SIZE),
-        hasNext: skip + DEFAULT_PAGE_SIZE < total,
-        hasPrev: skip > 0,
-      },
-      todos,
-    });
+    const todos = await TodoService.getTodos(req.user!.id, req.query);
+    return ApiResponse.success(res, todos);
   }
 
   static async getTodoById(req: Request<{ id: string }>, res: Response) {
-    const { id } = req.params;
-    const todo = await prisma.todo.findUnique({
-      where: { deletedAt: null, id, userId: req.user!.id },
-    });
+    const todo = await TodoService.getTodoById(req.user!.id, req.params.id);
 
-    if (!todo) {
-      return ApiResponse.notFound(res, "Todo not found");
-    }
+    if (!todo) return ApiResponse.notFound(res, "Todo not found");
 
     return ApiResponse.success(res, todo);
   }
@@ -93,15 +28,7 @@ export default class TodosController {
     req: Request<unknown, unknown, CreateTodoInput["body"]>,
     res: Response
   ) {
-    const { title, content } = req.body;
-
-    const todo = await prisma.todo.create({
-      data: {
-        title,
-        content,
-        userId: req.user!.id,
-      },
-    });
+    const todo = await TodoService.createTodo(req.user!.id, req.body);
 
     return ApiResponse.success(res, todo);
   }
@@ -110,40 +37,21 @@ export default class TodosController {
     req: Request<{ id: string }, unknown, UpdateTodoInput["body"]>,
     res: Response
   ) {
-    const { id } = req.params;
-    const { title, content, completed } = req.body;
+    const todo = await TodoService.updateTodo(
+      req.user!.id,
+      req.params.id,
+      req.body
+    );
 
-    const todoExists = await prisma.todo.findUnique({
-      where: { deletedAt: null, id, userId: req.user!.id },
-    });
-
-    if (!todoExists) {
-      return ApiResponse.notFound(res, "Todo not found");
-    }
-
-    const todo = await prisma.todo.update({
-      where: { id },
-      data: { title, content, completed },
-    });
+    if (!todo) return ApiResponse.notFound(res, "Todo not found");
 
     return ApiResponse.success(res, todo);
   }
 
   static async deleteTodo(req: Request<{ id: string }>, res: Response) {
-    const { id } = req.params;
+    const todo = await TodoService.deleteTodo(req.user!.id, req.params.id);
 
-    const todoExists = await prisma.todo.findUnique({
-      where: { deletedAt: null, id, userId: req.user!.id },
-    });
-
-    if (!todoExists) {
-      return ApiResponse.notFound(res, "Todo not found");
-    }
-
-    await prisma.todo.update({
-      where: { id },
-      data: { deletedAt: new Date() },
-    });
+    if (!todo) return ApiResponse.notFound(res, "Todo not found");
 
     return ApiResponse.success(res, null);
   }
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,102 @@
+import { DEFAULT_PAGE_SIZE } from "@/lib/core/options";
+import prisma from "@/lib/core/prisma";
+import {
+  CreateTodoInput,
+  GetTodosInput,
+  UpdateTodoInput,
+} from "@/lib/validations";
+
+export default class TodoService {
+  static async getTodos(userId: string, query: GetTodosInput["query"]) {
+    const { sort, q, page = 1 } = query;
+    const skip = (page - 1) * DEFAULT_PAGE_SIZE;
+
+    const [todos, total] = await Promise.all([
+      prisma.todo.findMany({
+        select: {
+          id: true,
+          title: true,
+          content: true,
+          completed: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+        where: {
+          deletedAt: null,
+          userId,
+          ...(q && {
+            OR: [
+              { title: { contains: q, mode: "insensitive" } },
+              { content: { contains: q, mode: "insensitive" } },
+            ],
+          }),
+        },
+        orderBy: {
+          ...(sort === "ascByCreatedAt" && { createdAt: "asc" }),
+          ...(sort === "descByCreatedAt" && { createdAt: "desc" }),
+          ...(sort === "ascByUpdatedAt" && { updatedAt: "asc" }),
+          ...(sort === "descByUpdatedAt" && { updatedAt: "desc" }),
+          ...(sort === "ascByCompleted" && { completed: "asc" }),
+          ...(sort === "descByCompleted" && { completed: "desc" }),
+          ...(sort === "ascByTitle" && { title: "asc" }),
+          ...(sort === "descByTitle" && { title: "desc" }),
+          ...(sort === "ascByContent" && { content: "asc" }),
+          ...(sort === "descByContent" && { content: "desc" }),
+        },
+        skip,
+        take: DEFAULT_PAGE_SIZE,
+      }),
+      prisma.todo.count({
+        where: {
+          deletedAt: null,
+          userId,
+          ...(q && {
+            OR: [
+              { title: { contains: q, mode: "insensitive" } },
+              { content: { contains: q, mode: "insensitive" } },
+            ],
+          }),
+        },
+      }),
+    ]);
+
+    return {
+      pagination: {
+        total,
+        totalPage: Math.ceil(total / DEFAULT_PAGE_SIZE),
+        hasNext: skip + DEFAULT_PAGE_SIZE < total,
+        hasPrev: skip > 0,
+      },
+      todos,
+    };
+  }
+
+  static async getTodoById(userId: string, id: string) {
+    return prisma.todo.findUnique({ where: { deletedAt: null, id, userId } });
+  }
+
+  static async createTodo(userId: string, data: CreateTodoInput["body"]) {
+    return prisma.todo.create({ data: { ...data, userId } });
+  }
+
+  static async updateTodo(
+    userId: string,
+    id: string,
+    data: UpdateTodoInput["body"]
+  ) {
+    const todoExists = await this.getTodoById(userId, id);
+    if (!todoExists) return null;
+
+    return prisma.todo.update({ where: { deletedAt: null, id, userId }, data });
+  }
+
+  static async deleteTodo(userId: string, id: string) {
+    const todoExists = await this.getTodoById(userId, id);
+    if (!todoExists) return null;
+
+    return prisma.todo.update({
+      where: { id },
+      data: { deletedAt: new Date() },
+    });
+  }
+}
