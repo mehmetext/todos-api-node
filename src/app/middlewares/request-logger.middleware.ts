e8d0f4de8ef8fe7e7248d1fe4e362344a98d0951
Mehmet <mehmetext68@gmail.com>
2024-11-18 14:32:58 +0300

    feat: add font styles to request-logger

--- src/app/middlewares/request-logger.middleware.ts
+++ src/app/middlewares/request-logger.middleware.ts
@@ -8,10 +8,20 @@ export default function requestLoggerMiddleware(
   const start = performance.now();
   const reqUrl = req.url;
 
+  const method = `${"\x1b[1m"}${req.method}${"\x1b[0m"}`;
+
+  let statusCode;
+
+  if (res.statusCode >= 200 && res.statusCode < 300) {
+    statusCode = `\x1b[32m${res.statusCode}\x1b[0m`;
+  } else {
+    statusCode = `\x1b[31m${res.statusCode}\x1b[0m`;
+  }
+
   res.on("finish", () => {
     const duration = performance.now() - start;
     console.log(
-      `${req.method} ${reqUrl} | ${res.statusCode} | ${duration.toFixed(2)}ms`
+      `${method} ➜ ${reqUrl} | ${statusCode} | ${duration.toFixed(2)}ms`
     );
   });
 
